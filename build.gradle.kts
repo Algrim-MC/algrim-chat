0b81a4ca1f920cd5fa6d23794d9b5f4731d70ad5
Algrim-MC <95324035+Algrim-MC@users.noreply.github.com>
2023-11-23 15:19:22 +0000

    Migrated Gradle to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,107 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("fabric-loom") version "1.3-SNAPSHOT"
+    id("maven-publish")
+    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+}
+
+version = project.properties["mod_version"]!!
+group = project.properties["maven_group"]!!
+
+base {
+    archivesName = project.properties["archives_base_name"].toString()
+}
+
+repositories {
+    maven { url = uri("https://masa.dy.fi/maven") }
+}
+
+loom {
+    splitEnvironmentSourceSets()
+
+    mods.create(project.name) {
+        sourceSet("client")
+    }
+}
+
+sourceSets {
+    test {
+        compileClasspath += sourceSets["client"].compileClasspath
+        compileClasspath += sourceSets["client"].output
+        runtimeClasspath += sourceSets["client"].runtimeClasspath
+        runtimeClasspath += sourceSets["client"].output
+    }
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
+    mappings("net.fabricmc:yarn:${project.properties["yarn_mappings"]}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
+
+    modImplementation("fi.dy.masa.malilib:malilib-fabric-${project.properties["minecraft_version"]}:${project.properties["malilib_version"]}")
+
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.properties["fabric_version"]}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:${project.properties["fabric_kotlin_version"]}")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.processResources {
+    inputs.property("mod_id", project.properties["mod_id"])
+    inputs.property("version", project.version)
+
+    filesMatching("fabric.mod.json") {
+        expand(
+            "mod_id" to project.properties["mod_id"],
+            "version" to project.version
+        )
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release = 17
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+tasks.jar {
+    from("LICENSE") {
+        rename { "${it}_${project.base.archivesName.get()}" }
+    }
+}
+
+java {
+    // Loom will automatically attach sourcesJar to a RemapSourcesJar task and to the "build" task
+    // if it is present.
+    // If you remove this line, sources will not be generated.
+    withSourcesJar()
+
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+// configure the maven publication
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+
+    // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
+    repositories {
+        // Add repositories to publish to here.
+        // Notice: This block does NOT have the same function as the block in the top level.
+        // The repositories here will be used for publishing your artifact, not for
+        // retrieving dependencies.
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = extra["mod_id"] as String
+pluginManagement {
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
